284ccf9b22bfda87dbd83bc639545d2beba74b6a
Roylu7384 <roylu7384@gmail.com>
2022-03-02 15:13:53 -0500

    Refactor createTweetElement function by using template literal to create html elements

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -22,41 +22,44 @@ $(() => {
       "user": {
         "name": "Descartes",
         "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
+        "handle": "@rd"
+      },
       "content": {
         "text": "Je pense , donc je suis"
       },
       "created_at": 1461113959088
     }
   ];
-  
+
   // function to create a single tweet element
-  const createTweetElement = function(tweetData) {
+  const createTweetElement = function (tweetData) {
     const $tweet = $(`<article class='tweets'></article>`);
-    const $header = $('<header>');
-    const $footer = $('<footer>');
-
-    const $profilePic = $(`<img src=${tweetData.user.avatars}>`);
-    const $userName = $('<span>').text(tweetData.user.name);
-    const $userHandle = $('<p>').text(tweetData.user.handle);
-    const $tweetText = $("<p class='tweet'>").text(tweetData.content.text);
-    const $createDate = $('<p>').text(timeago.format(tweetData.created_at));
-    const $socialIcons = $('<div>').html("<i class='fa-solid fa-flag'></i><i class='fa-solid fa-retweet'></i><i class='fa-solid fa-heart'></i>");
-
-    const $headerDiv = $('<div>').append($profilePic, $userName);
-    $header.append($headerDiv, $userHandle);
-    $tweet.append($header, $tweetText);
-
-    const $footerDiv1 = $('<div>').append($createDate);
-    const $footerDiv2 = $('<div>').append($socialIcons);
-    $footer.append($footerDiv1, $footerDiv2);
-
-    $tweet.append($footer);
+    const markup = `
+          <header>
+            <div>
+              <img src=${tweetData.user.avatars}> 
+              <span>${tweetData.user.name}</span>
+            </div>
+            <p>${tweetData.user.handle}</p>
+          </header>
+          <p class="tweet">${tweetData.content.text}</p>
+          <footer>
+            <div>
+              <p>${timeago.format(tweetData.created_at)}</p>
+            </div>
+            <div>
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-solid fa-retweet"></i>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+          </footer>
+    `;
+    $tweet.html(markup);
     return $tweet;
   };
 
   // function that take in an array of tweet data, create and append the tweet element to html
-  const renderTweets = function(tweets) {
+  const renderTweets = function (tweets) {
     const $tweetContainer = $('#tweets-container');
     let $tweetElement;
     for (const tweet of tweets) {
