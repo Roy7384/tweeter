65ae3ef5398150f4570553b83303373875aa86cf
Roylu7384 <roylu7384@gmail.com>
2022-03-02 21:27:21 -0500

    Implement scroll up botton to appear and also has same function as nav bar toggle

--- public/scripts/toggles.js
+++ public/scripts/toggles.js
@@ -1,5 +1,29 @@
 $(() => {
+  //helper function to check if textarea is visible on screen
+  const isTextareaVis = function () {
+    const offset = $('textarea')[0].getBoundingClientRect();
+    if (offset.top > 0) return true;
+    return false;
+  };
+
   $('nav button').click(() => {
+    $('textarea')[0].scrollIntoView(false);
+    $('textarea').focus();
+  });
+
+  $('#upButton').hide();
+
+  $(window).scroll(() => {
+    if (isTextareaVis()) {
+      $('nav div').fadeIn('slow');
+      $('#upButton').fadeOut('slow');
+    } else {
+      $('nav div').fadeOut('slow');
+      $('#upButton').fadeIn('slow');
+    }
+  });
+
+  $('#upButton').click(() => {
     $('textarea')[0].scrollIntoView(false);
     $('textarea').focus();
   });
